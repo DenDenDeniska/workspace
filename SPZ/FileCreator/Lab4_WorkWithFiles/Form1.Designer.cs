5c113b3c5326d5b00483341c9511c4cbbe177e20
Дмитрий <mr.osty@mail.ru>
2018-11-21 20:23:14 +0200

    spz lab4 complete

---
+++ SPZ/FileCreator/Lab4_WorkWithFiles/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Lab4_WorkWithFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btGenerateInput = new System.Windows.Forms.Button();
+            this.btGenericOut = new System.Windows.Forms.Button();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.tbOutFile = new System.Windows.Forms.TextBox();
+            this.btOpenOutFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btGenerateInput
+            // 
+            this.btGenerateInput.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.btGenerateInput.Location = new System.Drawing.Point(12, 12);
+            this.btGenerateInput.Name = "btGenerateInput";
+            this.btGenerateInput.Size = new System.Drawing.Size(148, 48);
+            this.btGenerateInput.TabIndex = 0;
+            this.btGenerateInput.Text = "Generic random txt ";
+            this.btGenerateInput.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btGenerateInput.UseVisualStyleBackColor = false;
+            this.btGenerateInput.Click += new System.EventHandler(this.btGenerateFile_Click);
+            // 
+            // btGenericOut
+            // 
+            this.btGenericOut.Location = new System.Drawing.Point(166, 12);
+            this.btGenericOut.Name = "btGenericOut";
+            this.btGenericOut.Size = new System.Drawing.Size(145, 48);
+            this.btGenericOut.TabIndex = 1;
+            this.btGenericOut.Text = "Generic  Output";
+            this.btGenericOut.UseVisualStyleBackColor = true;
+            this.btGenericOut.Click += new System.EventHandler(this.btGenericOut_Click);
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(12, 251);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(35, 13);
+            this.lbStatus.TabIndex = 2;
+            this.lbStatus.Text = "label1";
+            // 
+            // tbOutFile
+            // 
+            this.tbOutFile.Location = new System.Drawing.Point(12, 66);
+            this.tbOutFile.Multiline = true;
+            this.tbOutFile.Name = "tbOutFile";
+            this.tbOutFile.Size = new System.Drawing.Size(299, 143);
+            this.tbOutFile.TabIndex = 3;
+            // 
+            // btOpenOutFile
+            // 
+            this.btOpenOutFile.Location = new System.Drawing.Point(12, 215);
+            this.btOpenOutFile.Name = "btOpenOutFile";
+            this.btOpenOutFile.Size = new System.Drawing.Size(299, 23);
+            this.btOpenOutFile.TabIndex = 4;
+            this.btOpenOutFile.Text = "Open *.out";
+            this.btOpenOutFile.UseVisualStyleBackColor = true;
+            this.btOpenOutFile.Click += new System.EventHandler(this.btOpenOutFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 273);
+            this.Controls.Add(this.btOpenOutFile);
+            this.Controls.Add(this.tbOutFile);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.btGenericOut);
+            this.Controls.Add(this.btGenerateInput);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "FileWorker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btGenerateInput;
+        private System.Windows.Forms.Button btGenericOut;
+        private System.Windows.Forms.Label lbStatus;
+        private System.Windows.Forms.TextBox tbOutFile;
+        private System.Windows.Forms.Button btOpenOutFile;
+    }
+}
+
---
+++ SPZ/FileCreator/Lab4_WorkWithFiles/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Lab4_WorkWithFiles
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            lbStatus.Text = "Ожидание действий ...";
+            DirectoryInfo directory = new DirectoryInfo(@".\FilesForProject");
+            if (!directory.Exists)
+            {
+                directory.Create();
+                lbStatus.Text = "Директория успешно создана";
+            }
+
+            
+        }
+
+        private void btGenerateFile_Click(object sender, EventArgs e)
+        {
+            string path = @".\FilesForProject\InputData.txt";
+            FileInfo file = new FileInfo(path);
+            StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default);
+            Random rnd = new Random();
+            if (!file.Exists)
+            {
+                file.Create();
+                lbStatus.Text = "Файл успешно создан";
+            }
+            else
+            { 
+                int rndLength = rnd.Next(1, 100);
+                for (int i = 0; i < rndLength; i++)
+                {
+                    int rndOwnLength = rnd.Next(1, 25);
+                    for (int k = 0; k < rndOwnLength; k++)
+                    {
+                        sw.Write(rnd.NextDouble().ToString());
+                        //if (k != rndOwnLength - 1)
+                            sw.Write(",");
+                    }
+                    sw.WriteLine();
+                }
+                
+                sw.Close();
+                lbStatus.Text = "Генерация данных завершена";
+            }
+        }
+
+        private void btGenericOut_Click(object sender, EventArgs e)
+        {
+            string pathIn = @".\FilesForProject\InputData.txt";
+            string pathOut = @".\FilesForProject\InputData.out";
+            FileInfo fileInput = new FileInfo(pathIn);
+            if (!fileInput.Exists)
+            {
+                lbStatus.Text = "Файл InputData.txt отсутствует !!!";
+                return;
+            }
+            FileInfo fileOutput = new FileInfo(pathOut);
+            if (!fileInput.Exists)
+            {
+                fileOutput.Create();
+                lbStatus.Text = "Файл InputData.out создан";
+            }
+            StreamReader srInRd = new StreamReader(pathIn, System.Text.Encoding.Default);
+            StreamWriter srOutWr = new StreamWriter(pathOut, false ,System.Text.Encoding.Default);
+            
+            string line;
+            while ((line = srInRd.ReadLine()) != null)
+            {
+                int lineCounts = 0;
+                for (int i = 0; i < line.Length; i++)
+                    if (line[i] == ',')
+                        lineCounts++;
+
+                srOutWr.Write(lineCounts.ToString() + ',');
+            }
+            srInRd.Close();
+            srOutWr.Close();
+
+        }
+
+        private void btOpenOutFile_Click(object sender, EventArgs e)
+        {
+            string pathIn = @".\FilesForProject\InputData.out";
+            FileInfo fileOutput = new FileInfo(pathIn);
+            if (!fileOutput.Exists)
+            {
+                lbStatus.Text = "Файл InputData.out не найден !!!";
+                return;
+            }
+            StreamReader srInRd = new StreamReader(pathIn, System.Text.Encoding.Default);
+            
+            tbOutFile.AppendText(srInRd.ReadLine());
+          
+            srInRd.Close();
+        }
+    }
+}
